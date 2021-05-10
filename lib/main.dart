531331a79e3338d73831559921757f0b83744ea6
Mohit Jain <mohitjain9875@gmail.com>
2021-05-10 21:55:09 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text('Personal Expenses'),
+        title: Text('Personal   Expenses'),
       ),
       body: Text('Basic App'),
     ));
